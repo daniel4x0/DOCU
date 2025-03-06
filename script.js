f29156520468e9d61a0075e291c926a773e217d2
Daniel <danieluzegbu@gmail.com>
2025-03-06 17:43:21 +0000

    Updated html, style and js script - added JPEGs for carousel slides

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+// Tab Functionality
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
+    document.querySelectorAll(".tabcontent").forEach(tab => tab.style.display = "none");
     document.getElementById(tabName).style.display = "block";
-
     evt.currentTarget.className += " active";
 }
-
 document.getElementsByClassName("tablinks")[0].click();
+
+// Carousel Functionality
+let currentSlide = 0;
+function changeSlide(direction) {
+    let slides = document.getElementsByClassName("feature-slide");
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    slides[currentSlide].style.display = "block";
+}
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByClassName("feature-slide")[0].style.display = "block";
+});
+
+// Device Compatibility
+function toggleCompatibility() {
+    document.getElementById("device-compatibility").classList.toggle("hidden");
+}
+function openMiniTab(evt, tabName) {
+    document.querySelectorAll(".mini-tabcontent").forEach(tab => tab.classList.remove("active"));
+    document.getElementById(tabName).classList.add("active");
+}
