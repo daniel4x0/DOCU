eae3f431a50b35f7a8ecf3b7bc3244ddc9d4b339
Daniel <danieluzegbu@gmail.com>
2025-03-10 14:14:46 +0000

    removed carousel functinality from players tab and added image expand function - updated FAQs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Tab Functionality
+
 function openTab(evt, tabName) {
     document.querySelectorAll(".tabcontent").forEach(tab => tab.style.display = "none");
     document.getElementById(tabName).style.display = "block";
@@ -6,7 +6,6 @@ function openTab(evt, tabName) {
 }
 document.getElementsByClassName("tablinks")[0].click();
 
-// Carousel Functionality
 let currentSlide = 0;
 function changeSlide(direction) {
     let slides = document.getElementsByClassName("feature-slide");
@@ -18,11 +17,24 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementsByClassName("feature-slide")[0].style.display = "block";
 });
 
-// Device Compatibility
+function expandImage(img) {
+    document.getElementById("expanded-img").src = img.src;
+    document.getElementById("expanded-container").classList.remove("hidden");
+}
+
+function closeExpanded() {
+    document.getElementById("expanded-container").classList.add("hidden");
+}
+
 function toggleCompatibility() {
     document.getElementById("device-compatibility").classList.toggle("hidden");
 }
 function openMiniTab(evt, tabName) {
     document.querySelectorAll(".mini-tabcontent").forEach(tab => tab.classList.remove("active"));
     document.getElementById(tabName).classList.add("active");
+}
+
+function toggleFAQ(question) {
+    let answer = question.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
 }
